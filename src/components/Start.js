1c6cbb8b78ef4fcffcb46e901826407280d53c5e
Work desktop <the.red.lemon@yandex.ru>
2023-09-15 15:18:32 +0300

    feat: adds styles and initial func for start component

--- src/components/Start.js
+++ src/components/Start.js
@@ -1,6 +1,24 @@
 class Start {
-  constructor() {
+  constructor(setting) {
+    this._setting = setting;
+    this._form = document.querySelector('#start-form');
+    this._startBtn = this._form.querySelector('.start__btn[data-type="start"]');
+    this._firstNum = this._form.querySelector('.start__input[data-type="first"]');
+    this._lastNum = this._form.querySelector('.start__input[data-type="last"]');
+  }
 
+  _startGame = () => {
+
+  }
+
+  setEnentListeners = () => {
+    this._startBtn.addEventListener('click', () => {
+      if (
+        this._firstNum < this._lastNum && this._firstNum >= 1
+      ) {
+
+      }
+    });
   }
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,7 @@
 import './index.css';
+
+import Start from '../components/Start';
+
+const start = new Start();
+
+start.setEnentListeners();
